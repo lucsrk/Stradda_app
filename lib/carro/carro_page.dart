47646af50b4016901fba4186a25cfba9f861622b
lucrsk <lucas.srsantos1@gmail.com>
2021-03-28 01:09:10 -0300

    page da descricao

--- lib/carro/carro_page.dart
+++ lib/carro/carro_page.dart
@@ -13,6 +13,29 @@ class CarroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text (carro.nome),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+          icon: Icon(Icons.videocam),
+          onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopupMenu,
+            itemBuilder: (BuildContext context){
+            return[
+              PopupMenuItem(value: "Compartilhar",
+                  child: Text ("Compartilhar"),),
+              PopupMenuItem(value: "Editar",
+                child: Text ("Editar"),),
+              PopupMenuItem(value: "Deletar",
+                child: Text ("Deletar"),),
+            ];
+          },
+          )
+      ],
       ),
       body: _body()
     );
@@ -21,6 +44,89 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-        child: Image.network(carro.urlFoto));
+        child: ListView(
+          children: [
+            Image.network(carro.urlFoto),
+            Divider(),
+            _bloco1(),
+            _bloco2(),
+          ],
+        ));
+  }
+
+  Row _bloco1() {
+    return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
+                  Text(carro.nome,
+                    style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold ),),
+                  Text(carro.tipo, style: TextStyle(fontSize: 16),),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.favorite, size: 40,),
+                    onPressed: _onClickFavorito,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share, size: 40,),
+                    onPressed: _onClickShare,
+                  ),
+                ],
+              ),
+            ],
+          );
+  }
+
+  _bloco2(){
+    return Column(
+      crossAxisAlignment:  CrossAxisAlignment.start,
+      children: [
+        SizedBox( height: 15,),
+        Text (carro.descricao, style: TextStyle (fontSize: 16, fontWeight: FontWeight.bold), ),
+        SizedBox( height: 20,),
+        Text ("ttttt", style: TextStyle (fontSize: 16),),
+
+
+
+      ],
+
+
+    );
+
+
+  }
+
+  void _onClickMapa() {
+  }
+
+  void _onClickVideo() {
+  }
+
+  _onClickPopupMenu(String value) {
+    switch(value){
+      case "Compartilhar":
+        print ("Compartilhar !!!");
+        break;
+      case "Editar":
+        print ("Editar !!!");
+        break;
+      case "Deletar":
+        print ("Deletar !!!");
+        break;
+
+    }
+
+  }
+
+  void _onClickFavorito() {
+  }
+
+  void _onClickShare() {
   }
 }
