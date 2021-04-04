c9a3368fd4c9df839650da850e076824e12aac19
lucrsk <lucas.srsantos1@gmail.com>
2021-04-04 17:54:01 -0300

    ajustes genericos

--- lib/favoritos/carros_dao.dart
+++ lib/favoritos/carros_dao.dart
@@ -2,72 +2,29 @@ import 'dart:async';
 
 import 'package:sqflite/sqflite.dart';
 import 'package:stradda_01/carros/carro.dart';
+import 'package:stradda_01/favoritos/base_dao.dart';
 import 'package:stradda_01/favoritos/db_helper.dart';
 
 // Data Access Object
-class CarroDAO {
+class CarroDAO extends BaseDAO <Carro>{
+  @override
 
-  Future<Database> get db => DatabaseHelper.getInstance().db;
+  String get tableName => "carro" ;
 
-  Future<int> save(Carro carro) async {
-    var dbClient = await db;
-    var id = await dbClient.insert("carro", carro.toJson(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-    print('id: $id');
-    return id;
-  }
+  @override
+  Carro fromMap(Map<String, dynamic> map) {
+    return Carro.fromMap(map);
 
-  Future<List<Carro>> findAll() async {
-    final dbClient = await db;
-
-    final list = await dbClient.rawQuery('select * from carro');
-
-    final carros = list.map<Carro>((json) => Carro.fromJson(json)).toList();
-
-    return carros;
   }
 
   Future<List<Carro>> findAllByTipo(String tipo) async {
     final dbClient = await db;
 
-    final list = await dbClient.rawQuery('select * from carro where tipo =? ',[tipo]);
+    final list = await dbClient.rawQuery('select * from $tableName where tipo =? ',[tipo]);
 
-    final carros = list.map<Carro>((json) => Carro.fromJson(json)).toList();
+    return list.map<Carro>((json) => fromMap(json)).toList();
 
-    return carros;
   }
 
-  Future<Carro> findById(int id) async {
-    var dbClient = await db;
-    final list =
-    await dbClient.rawQuery('select * from carro where id = ?', [id]);
 
-    if (list.length > 0) {
-      return new Carro.fromJson(list.first);
-    }
-
-    return null;
-  }
-
-  Future<bool> exists(Carro carro) async {
-    Carro c = await findById(carro.id);
-    var exists = c != null;
-    return exists;
-  }
-
-  Future<int> count() async {
-    final dbClient = await db;
-    final list = await dbClient.rawQuery('select count(*) from carro');
-    return Sqflite.firstIntValue(list);
-  }
-
-  Future<int> delete(int id) async {
-    var dbClient = await db;
-    return await dbClient.rawDelete('delete from carro where id = ?', [id]);
-  }
-
-  Future<int> deleteAll() async {
-    var dbClient = await db;
-    return await dbClient.rawDelete('delete from carro');
-  }
 }
