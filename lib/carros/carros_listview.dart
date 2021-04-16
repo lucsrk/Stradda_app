c2102aeb40a7284f3fd02583a6a9bea6ab170a83
lucrsk <lucas.srsantos1@gmail.com>
2021-04-16 01:37:12 -0300

    compartilhar ativo

--- lib/carros/carros_listview.dart
+++ lib/carros/carros_listview.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 import 'package:stradda_01/carros/carro.dart';
 import 'package:stradda_01/carros/carro_page.dart';
 import 'package:stradda_01/carros/carros_api.dart';
@@ -24,56 +25,67 @@ class CarrosListView extends StatelessWidget {
           itemBuilder: (context, index) {
             Carro c = carros[index];
 
-            return Card(
-              color: Colors.grey[200],
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Center(
-                      child: CachedNetworkImage(
-                        imageUrl:
-                         c.urlFoto
-                            ?? "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/luxo/Pagani_Zonda.png",
-                        width: 250,
+            return Container(
+              child: InkWell(
+                onTap: (){
+                  _OnClickCarro(context, c);
+                },
+                onLongPress:() {
+                  _onLongClickCarro(context, c);
+                },
+              child: Card(
+                color: Colors.grey[200],
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                        child: CachedNetworkImage(
+                          imageUrl:
+                           c.urlFoto
+                              ?? "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/luxo/Pagani_Zonda.png",
+                          width: 250,
+                        ),
                       ),
-                    ),
-                    Text(
-                      c.nome,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 22),
-                    ),
-                    Text(
-                      "Descrição",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    ButtonBarTheme(
-                      data: ButtonBarTheme.of(context),
-                      child: ButtonBar(
-                        children: <Widget>[
-                          TextButton(
-                            child: const Text(
-                              'Detalhes',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            onPressed: () => _OnClickCarro(context, c),
-                          ),
-                          TextButton(
-                            child: const Text(
-                              'Compartilhar',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            onPressed: () {
-                              /* ... */
-                            },
-                          ),
-                        ],
+                      Text(
+                        c.nome,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 22),
                       ),
-                    ),
-                  ],
+                      Text(
+                        "Descrição",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      ButtonBarTheme(
+                        data: ButtonBarTheme.of(context),
+                        child: ButtonBar(
+                          children: <Widget>[
+                            TextButton(
+                              child: const Text(
+                                'Detalhes',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              onPressed: () => _OnClickCarro(context, c),
+                            ),
+                            TextButton(
+                              child: const Text(
+                                'Compartilhar',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              onPressed: () {
+                                _OnClickShare(context, c);
+                                /* ... */
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ),
               ),
             );
           }),
@@ -83,5 +95,42 @@ class CarrosListView extends StatelessWidget {
   _OnClickCarro(context, Carro c) {
     push (context, CarroPage(c));
 
+  }
+
+  void _onLongClickCarro(BuildContext context, Carro c) {
+    showModalBottomSheet(context: context, builder: (context){
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget> [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(c.nome, style: TextStyle (fontSize: 22, fontWeight: FontWeight.bold),),
+          ),
+          ListTile(
+            title:  Text("Detalhes"),
+            leading: Icon(Icons.directions_car),
+            onTap: (){
+              Navigator.pop(context);
+              _OnClickCarro(context, c);
+            },
+          ),
+          ListTile(
+            title:  Text("Compartilhar"),
+              leading: Icon (Icons.share),
+              onTap:() {
+                Navigator.pop(context);
+                _OnClickShare(context, c);
+              }
+          )
+
+        ],
+      );
+    });
+  }
+
+  void _OnClickShare(BuildContext context, Carro c) {
+    print ("Share ${c.nome}");
+
+    Share.share(c.urlFoto);
   }
 }
